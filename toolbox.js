64c189f75e6a8aed37b0358fb1bc364315ab7d29
PhilippePerret <philippe.perret@yahoo.fr>
2023-04-11 17:56:56 +0200

    Isolation dans la toolbox des résultats KDP

--- toolbox.js
+++ toolbox.js
@@ -16,6 +16,15 @@ Toolbox.onToggleCheckKDP = function(ev){
 // Les objets
 const btnGotoKDP = DCreate('BUTTON',{text:"Rejoindre KDP"})
 const cbCheckKDP = DCreate('INPUT', {type:'checkbox', id:'cb-check-kdp', label:'Vérifier les ventes KDP'})
+const styleSpanKDP = 'float:left;font-size:20pt;font-weight:bold;color:white;margin-top: 6px;margin-left: 12px;'
+const spanKDP = DCreate('SPAN', {id:'kdp-resultats', style:styleSpanKDP})
+;spanKDP.appendChild(DCreate('SPAN',{text:'DKP : '}))
+;spanKDP.appendChild(DCreate('SPAN',{id:'kdp-nombre-ventes', text:'---'}))
+spanKDP.appendChild(DCreate('SPAN',{text:' / '}))
+spanKDP.appendChild(DCreate('SPAN',{id:'kdp-moyenne-ventes',text:'---'}))
+const styleSpanTime = 'margin-left:1em;font-size:12pt;vertical-align:middle;'
+spanKDP.appendChild(DCreate('DIV',{id:'kdp-time',text:'', style:styleSpanTime}))
+
 
 // Les observers
 listen(btnGotoKDP,'click',Toolbox.gotoKDP.bind(Toolbox))
@@ -23,8 +32,9 @@ listen(DGet('input',cbCheckKDP),'change', Toolbox.onToggleCheckKDP.bind(Toolbox)
 
 Toolbox.data = {
   buttons: [
-    cbCheckKDP,
-    btnGotoKDP,
+      cbCheckKDP
+    , spanKDP
+    , btnGotoKDP
   ]
 }
 
@@ -39,8 +49,8 @@ class KDP {
     this.kdpTimer && clearTimeout(this.kdpTimer)
     if (undefined == retour) {
       // TODO Il faut pouvoir insérer des éléments dans l'interface
-      DGet('span#kdp-nombre-ventes').innerHTML = `…`
-      DGet('span#kdp-time').innerHTML = DateUtils.currentTime()
+      this.spanNombreVentes.innerHTML = `…`
+      this.spanTime.innerHTML = '(' + DateUtils.currentTime() + ')'
       WAA.send({class:"Dashboard::Toolbox",method:"get_kdp_score", data:{ok:true}})
     } else {
       if ( retour.ok ) {
@@ -49,10 +59,11 @@ class KDP {
         */
         const oldNombreVentes = Number(retour.oldNombreVentes)
         const newNombreVentes = Number(retour.newNombreVentes)
-        DGet('span#kdp-nombre-ventes').innerHTML = `${newNombreVentes}`
-        DGet('span#kdp-time').innerHTML = DateUtils.currentTime()
+        this.spanNombreVentes.innerHTML = `${newNombreVentes}`
+        this.spanMoyenne.innerHTML = this.calcMoyenneVentes(newNombreVentes)
+        this.spanTime.innerHTML = '(' + DateUtils.currentTime() + ')'
         this.kdpTimer = setTimeout(this.getKDPResult.bind(this), 5 * 60 * 1000 /* toutes les x minutes */)
-        console.log("nombreVentes = ", newNombreVentes, DateUtils.currentTime())
+        // console.log("nombreVentes = ", newNombreVentes, DateUtils.currentTime())
       } else {
         /*
         |  Erreur:
@@ -60,6 +71,29 @@ class KDP {
         erreur(retour.msg)
       }
     }
+  }
+
+  // --- Les objets DOM ---
+
+  static get spanNombreVentes(){
+    return this._spanventes || (this._spanventes = DGet('span#kdp-nombre-ventes'))
+  }
+  static get spanMoyenne(){
+    return this._spanmoy || (this._spanmoy = DGet('span#kdp-moyenne-ventes'))
+  }
+  static get spanTime(){
+    return this._spantime || (this._spantime = DGet('div#kdp-time'))
+  }
+
+  /**
+  * Méthode qui calcule la moyenne des ventes du mois courant
+  * et la @return.
+  */
+  static calcMoyenneVentes(nombreVentes){
+    const nombreJours = new Date().getDate();
+    let moyenne = Number.parseFloat(nombreVentes / nombreJours).toFixed(1)
+    // console.log("Moyenne : ", moyenne)
+    return moyenne
   }
 
 }
